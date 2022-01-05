6d08e778e0e9fcf69cb261a8d862402bc783efdf
Hey-Deepak <choudharydeepak990@gmail.com>
2022-01-05 23:58:45 +0530

    NotesDatabase

--- src/Application.kt
+++ src/Application.kt
@@ -1,11 +1,23 @@
 package com.androiddevs
 
-import io.ktor.application.Application
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
+import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing)
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+        }
+    }
+
 }
 
---
+++ src/data/NotesDatabase.kt
@@ -0,0 +1,4 @@
+package com.androiddevs.data
+
+class NotesDatabase {
+}
