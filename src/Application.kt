8b5d04de7a2dce56c8d6708a3248db2e7fb302c8
Hey-Deepak <choudharydeepak990@gmail.com>
2022-01-06 16:00:56 +0530

    Setup Done

--- src/Application.kt
+++ src/Application.kt
@@ -1,11 +1,26 @@
 package com.androiddevs
 
+import com.androiddevs.data.collections.User
+import com.androiddevs.data.registerUser
 import io.ktor.application.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    CoroutineScope(Dispatchers.IO).launch {
+        registerUser(
+            User(
+                "deepak@gmail",
+                "12345"
+            )
+        )
+    }
+
 }
 
